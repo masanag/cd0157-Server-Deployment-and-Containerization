cec23e2e12ee5e26d46d4b85b1d8301742e5457e
masanag <masaki.nagaishi@gmail.com>
2023-10-20 15:38:33 +0900

    Add bad test to test_main.py to check codebuild response

--- test_main.py
+++ test_main.py
@@ -26,15 +26,17 @@ def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
+    assert False
 
 
 def test_auth(client):
     body = {'email': EMAIL,
             'password': PASSWORD}
-    response = client.post('/auth', 
+    response = client.post('/auth',
                            data=json.dumps(body),
                            content_type='application/json')
 
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
+    assert False
